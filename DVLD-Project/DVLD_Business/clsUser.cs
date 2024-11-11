c9ec9839d56946b3f87d93f74f24763a2eb917f2
AnasMohammed212 <110040495+AnasMohammed212@users.noreply.github.com>
2024-11-11 17:07:44 +0300

    User business Layer

--- DVLD-Project/DVLD_Business/clsUser.cs
+++ DVLD-Project/DVLD_Business/clsUser.cs
@@ -10,6 +10,95 @@ namespace DVLD_Business
 {
     public class clsUser
     {
+        public enum enMode { AddNew=0, Update=1};
+        public enMode Mode=enMode.AddNew;
+        public int UserID {  get; set; }
+        public int PersonID { get; set; }
+        public clsPerson PersonInfo;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public clsUser()
+        {
+            this.UserID = -1;
+            this.UserName = "";
+            this.Password = "";
+            this.IsActive = false;
+            Mode= enMode.AddNew;
+        }
+        private clsUser(int UserID,int PersonID,string UserName,string Password,bool IsActive)
+        {
+            this.UserID=UserID;
+            this.PersonID=PersonID;
+            this.PersonInfo=clsPerson.Find(PersonID);
+            this.UserName=UserName;
+            this.Password=Password;
+            this.IsActive =IsActive;
+            Mode= enMode.Update;
+        }
+        public static clsUser FindByUserID(int UserID)
+        {
+            int PersonID = -1;
+            string UserName = "", Password = "";
+            bool IsActive = false;
+            bool IsFound=clsUserData.GetUserInfoByUserID(UserID,ref PersonID,ref UserName,ref Password,ref IsActive);
+            if(IsFound)
+                return new clsUser(UserID,PersonID,UserName,Password,IsActive);
+            else
+                return null;
+        }
+        public static clsUser FindByUserNameAndPassword(string UserName,string Password)
+        {
+            int UserID = -1,PersonID=-1;
+            bool IsActive = false;
+            bool IsFound = clsUserData.GetUserInfoByUsernameAndPassword(UserName,Password,ref UserID,ref PersonID,ref IsActive);
+            if (IsFound)
+                return new clsUser(UserID, PersonID, UserName, Password, IsActive);
+            else
+                return null;
+        }
+        private bool _AddNewUser()
+        {
+            this.UserID = clsUserData.AddNewUser(this.PersonID,this.UserName,this.Password,this.IsActive);
+            return (this.UserID!=-1);
+        }
+        private bool _UpdateUser()
+        {
+            return clsUserData.UpdateUser(this.UserID,this.PersonID,this.UserName,this.Password,this.IsActive);
+        }
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewUser())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                        return false;
+                case enMode.Update:
+                    return _UpdateUser();
+            }
+            return false;
+        }
+        public static bool DeleteUser(int UserID)
+        {
+            return clsUser.DeleteUser(UserID);
+        }
+        public static bool IsUserExist(int UserID)
+        {
+            return clsUserData.IsUserExist(UserID);
+        }
+        public static bool IsUserExist(string UserName)
+        {
+            return clsUserData.IsUserExist(UserName);
+        }
+        public static bool IsUserExistForPersonID(int PersonID)
+        {
+            return clsUserData.IsUserExistForPersonID(PersonID);
+        }
         public static DataTable GetAllUsers()
         {
             return clsUserData.GetAllUsers();
