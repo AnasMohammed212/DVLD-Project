c8b98db6cea9f1438c9c93e177e72c38957a89cb
AnasMohammed212 <110040495+AnasMohammed212@users.noreply.github.com>
2025-03-20 23:37:42 +0300

    .

--- DVLD-Project/DVLD/Properties/Resources.Designer.cs
+++ DVLD-Project/DVLD/Properties/Resources.Designer.cs
@@ -523,16 +523,6 @@ namespace DVLD.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap IconLogo {
-            get {
-                object obj = ResourceManager.GetObject("IconLogo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap International_32 {
             get {
                 object obj = ResourceManager.GetObject("International 32", resourceCulture);
